fbb2f8c9a1825d70cced135f50e23f6e0b95852c
Tuan Dung Nguyen <dzung.csu@gmail.com>
2026-02-07 22:18:47 -0700

    version name change to 2.1.03

--- src/prism_root/Prism2Main.java
+++ src/prism_root/Prism2Main.java
@@ -94,7 +94,7 @@ public class Prism2Main extends JFrame {
 	private MenuItem_SetTransparency 	setTransparency;		// For menuWindow
 	private MenuItem_CaptureGUI 		captureGUI;				// For menuWindow
 		
-	private static String 				prism_version = "PRISM ALPHA 2.1.01";
+	private static String 				prism_version = "PRISM ALPHA 2.1.03";
 	private String 						currentProject;
 	private static PrismDesktopPane 	prism_DesktopPane;
 	private static PrismContentPane 	prism_ContentPane;
